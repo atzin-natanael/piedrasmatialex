4f8fdb3098f4930389d658ac8c668324faa47827
Atzin Natanael Pacheco Arellano <80654120+atzin-natanael@users.noreply.github.com>
2023-04-25 01:21:45 -0600

    nuevo

---
+++ js/scriptstr.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.articulostr img').forEach(image =>{
+    image.onclick = () =>  {
+        document.querySelector('.popup-img').style.display = 'block';
+        document.querySelector('.popup-img img').src = image.getAttribute('src');
+    }
+});
+document.querySelector('.popup-img span').onclick = () => {
+    document.querySelector('.popup-img').style.display = 'none'; 
+}
+document.querySelector('.contenedor .popup-img').onclick = () => {
+    document.querySelector('.popup-img').style.display = 'none'; 
+}
