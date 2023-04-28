d54ce06b611d2d1a32f370fbcde382fc091523fd
Atzin Natanael Pacheco Arellano <80654120+atzin-natanael@users.noreply.github.com>
2023-04-28 03:03:49 -0600

    slider

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,12 @@ document.querySelectorAll('.articulos img').forEach(image =>{
         document.querySelector('.popup-img img').src = image.getAttribute('src');
     }
 });
+document.querySelectorAll('.mySwiper img').forEach(image =>{
+    image.onclick = () =>  {
+        document.querySelector('.popup-img').style.display = 'block';
+        document.querySelector('.popup-img img').src = image.getAttribute('src');
+    }
+});
 document.querySelector('.popup-img span').onclick = () => {
     document.querySelector('.popup-img').style.display = 'none'; 
 }
