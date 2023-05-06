ccdf9ada835aa0f6069457ed6ae0050d2445dd8f
Atzin Natanael Pacheco Arellano <80654120+atzin-natanael@users.noreply.github.com>
2023-05-06 01:55:22 -0600

    float

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,16 @@ document.querySelector('.popup-img span').onclick = () => {
 document.querySelector('.contenedor .popup-img').onclick = () => {
     document.querySelector('.popup-img').style.display = 'none'; 
 }
+
+const nav = document.querySelector(".float");
+  let lastScrollY = window.scrollY;
+
+  window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+      nav.classList.add("nav--hidden");
+    } else {
+      nav.classList.remove("nav--hidden");
+    }
+
+    lastScrollY = window.scrollY;
+  });
